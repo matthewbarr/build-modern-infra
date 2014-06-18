c72aa54292f7edb8afb82b4768e4621c6383e0da
Matthew Barr <mbarr@snap-interactive.com>
2014-06-18 00:24:09 -0400

    Started in on the main code base.  site.pp

---
+++ environments/production/manifests/site.pp
@@ -0,0 +1,36 @@
+# Default file attributes
+File {
+  ignore => [ '.svn', '.git', 'CVS', '.bzr' ],
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+} # File
+
+# Default package attribute
+
+Package {
+  ensure => installed,
+} # Package
+
+# Default exec paths
+
+Exec {
+  path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+} # Exec
+
+node default {
+  if $role == "mco" {
+    class {'mcollective':
+      middleware       => true,
+      client           => true,
+      connector        => 'rabbitmq',
+    }
+#    include profile::sensu::server
+  }
+  if $role == "search" {
+    include profile::elasticsearch
+  }
+  if $role == "logstash" {
+    include profile::logstash
+  }
+}
