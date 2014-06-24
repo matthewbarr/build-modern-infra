9b3a1e42de90a8f7d8392455e9f3fb11361427c3
Matthew Barr <mbarr@snap-interactive.com>
2014-06-24 01:00:10 -0400

    Added sensu client

---
+++ environments/production/modules-local/profiles/manifests/sensu/client.pp
@@ -0,0 +1,14 @@
+# Class: profiles::sensu::client
+#
+#
+class profiles::sensu::client {
+  class { 'sensu':
+    rabbitmq_password        => 'meep',
+    rabbitmq_ssl_cert_chain  => 'puppet:///modules/kenshosensu/client/cert.pem',
+    rabbitmq_ssl_private_key => 'puppet:///modules/kenshosensu/client/key.pem',
+    subscriptions            => 'kensho',
+    rabbitmq_port            => '5671',
+    use_embedded_ruby        => true,
+    rabbitmq_host            => 'rabbitmq.aws.mbarr.net'
+    }
+}
