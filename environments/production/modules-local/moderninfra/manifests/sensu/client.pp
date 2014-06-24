51a5492c6fd0f12f2aab995820f80506a3b3795b
Matthew Barr <mbarr@snap-interactive.com>
2014-06-24 16:46:13 -0400

    Comment out the ssl certs for sensu client.

--- environments/production/modules-local/moderninfra/manifests/sensu/client.pp
+++ environments/production/modules-local/moderninfra/manifests/sensu/client.pp
@@ -2,19 +2,19 @@
 #
 #
 class moderninfra::sensu::client {
-  file { '/etc/sensu/ssl/cert.pem':
-    owner   => 'sensu',
-    group   => 'sensu',
-    mode   => '0444',
-    source => "file:///var/lib/puppet/ssl/certs/${fqdn}.pem";
-  }
-
-  file { '/etc/sensu/ssl/key.pem':
-    owner   => 'sensu',
-    group   => 'sensu',
-    mode   => '0400',
-    source => "file:///var/lib/puppet/ssl/private_keys/${fqdn}.pem";
-  } 
+  # file { '/etc/sensu/ssl/cert.pem':
+  #   owner   => 'sensu',
+  #   group   => 'sensu',
+  #   mode   => '0444',
+  #   source => "file:///var/lib/puppet/ssl/certs/${fqdn}.pem";
+  # }
+  #
+  # file { '/etc/sensu/ssl/key.pem':
+  #   owner   => 'sensu',
+  #   group   => 'sensu',
+  #   mode   => '0400',
+  #   source => "file:///var/lib/puppet/ssl/private_keys/${fqdn}.pem";
+  # }
   class { 'sensu':
     rabbitmq_password        => 'meep',
 #    rabbitmq_ssl_cert_chain  => '/etc/sensu/ssl/cert.pem',
