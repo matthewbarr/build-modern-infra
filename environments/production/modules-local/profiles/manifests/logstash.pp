91c90174f62c1b1ca172deb452de30ee0075e4b7
Matthew Barr <mbarr@snap-interactive.com>
2014-06-24 16:51:00 -0400

    Added elasticsearch directly to logstash profile.

--- environments/production/modules-local/profiles/manifests/logstash.pp
+++ environments/production/modules-local/profiles/manifests/logstash.pp
@@ -3,13 +3,18 @@
 #
 class profiles::logstash {
   include moderninfra::logstash::server
-  include moderninfra::elasticsearch
   
   
   logstash::configfile { 'basic_config':
-    source => 'puppet:///profiles/logstash/basic_config',
+    source => 'puppet:///modules/profiles/logstash/basic_config',
     order  => 10
   }  
+
+  class { 'elasticsearch':
+    java_install  => true,
+    manage_repo   => true,
+    repo_version  => '1.1',
+  }
 
 # Inputs
   # logstash::input::syslog { 'logstash-syslog':
